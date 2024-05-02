de8e642a8b2147112caadce435d0e118a07e4747
wanjohi lewis <wanjohilewi7@gmail.com>
2024-05-02 18:27:12 +0300

    my springboot Rest API controller

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,12 +3,13 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@RequestMapping("/api/v1/person")
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
 
@@ -20,7 +21,7 @@ public class PersonController {
     }
 
     @PostMapping
-    public void addPerson(@RequestBody Person person) {
+    public void addPerson(@Valid @NonNull  @RequestBody Person person) {
         personService.addPerson(person);
     }
 
@@ -32,5 +33,14 @@ public class PersonController {
     public Person getPersonById(@PathVariable("id")UUID id){
         return personService.getPersonById(id)
                 .orElse(null);
+    }
+    @DeleteMapping(path = "{id}")
+    public void deletePersonById(@PathVariable("id")UUID id){
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updatePerson(@PathVariable("id") UUID id,@RequestBody Person personToUpdate){
+        personService.updatePerson(id, personToUpdate);
     }
 }
